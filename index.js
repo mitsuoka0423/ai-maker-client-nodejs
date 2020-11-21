4429716e8cecaa8bde2aa6c01b40cf01bb8a4daa
takahiro mitsuoka <tmitsuoka0423@gmail.com>
2020-11-21 21:12:24 +0900

    update

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ const fs = require('fs/promises');
 const main = async () => {
     const file = await fs.readFile('./IMG_0533.jpg');
 
-    const response = await axios.post('https://aimaker.io/image/classification/api', {
-        id: process.env.AI_MAKER_ID,
-        apikey: process.env.AI_MAKER_API_KEY,
-        base64: file.toString('base64'),
-    });
+    const params = new URLSearchParams();
+    params.append('id', process.env.AI_MAKER_ID);
+    params.append('apikey', process.env.AI_MAKER_API_KEY);
+    params.append('base64', file.toString('base64'));
+
+    const response = await axios.post('https://aimaker.io/image/classification/api', params);
     console.log(response.data);
 }
 
